7b491be8d35bcd38d986c9a62214fdaf6b25de9c
zpawn <ilia.makarov@me.com>
2017-08-15 22:23:54 +0300

    - styled fullscreen menu - close on press esc

--- royalrangers/src/js/hamburger.js
+++ royalrangers/src/js/hamburger.js
@@ -1,10 +1,22 @@
 (function ($) {
     "use strict";
 
-    $(document).ready(function(){
-        $('.hamburger').click(function(){
-            $(this).toggleClass('open');
-            $('.menu').toggleClass('open');
+    $(document).ready(function () {
+
+        $('.hamburger').click(function () {
+            toggleMenu();
+        });
+
+        $(document).on('keyup', function (e) {
+            if (e.keyCode === 27 && $(document.body).hasClass('menu-content-open')) {
+                toggleMenu();
+            }
         });
     });
+
+    function toggleMenu () {
+        $('.hamburger').toggleClass('open');
+        $('.menu-content').toggleClass('open');
+        $(document.body).toggleClass('menu-content-open');
+    }
 }(jQuery));
