172fcf6365fcedff45e7e75ce8761a7166a6eed9
zpawn <ilia.makarov@me.com>
2017-11-26 02:09:20 +0200

    Add link to index page

--- todomanager/src/js/Store.class.js
+++ todomanager/src/js/Store.class.js
@@ -34,7 +34,6 @@
                 Mediator.publish('show', 'spinner');
 
                 req.open(method, url, true);
-                // req.withCredentials = true;
                 req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
                 req.onload = () => {
                     if (req.status === 200) {
