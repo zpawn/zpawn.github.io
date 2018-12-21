689d9efba69b4f44f901623c9896014d1fe50109
zpawn <ilia.makarov@me.com>
2018-12-22 01:01:32 +0200

    Updates

---
+++ precache-manifest.c1085d85c1df747787973d4fd49cc46f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5657ccbd46520bc3b431",
+    "url": "/my-english/static/js/runtime~main.5657ccbd.js"
+  },
+  {
+    "revision": "1145e78bc72d4bcb2aa3",
+    "url": "/my-english/static/js/main.1145e78b.chunk.js"
+  },
+  {
+    "revision": "406d87afa3507663982f",
+    "url": "/my-english/static/js/1.406d87af.chunk.js"
+  },
+  {
+    "revision": "1145e78bc72d4bcb2aa3",
+    "url": "/my-english/static/css/main.08dece92.chunk.css"
+  },
+  {
+    "revision": "3d0049708ee0d2dbc40830b563c5f532",
+    "url": "/my-english/index.html"
+  }
+];
