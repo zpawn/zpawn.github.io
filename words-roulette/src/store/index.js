2be325419bd2b3c3e4bbbceaf2762ba26cbdc773
zpawn <ilia.makarov@me.com>
2019-01-06 22:51:49 +0200

    WordsRoulette: connect redux-thunk

--- words-roulette/src/store/index.js
+++ words-roulette/src/store/index.js
@@ -1,10 +1,10 @@
-import { createStore /*applyMiddleware*/ } from "redux";
-// import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import rootReducer from "./rootReducer";
 
 ////
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export { store };
