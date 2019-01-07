fa79a5e11a0fb740486675595618aa2515ba6729
zpawn <ilia.makarov@me.com>
2019-01-07 03:07:25 +0200

    WordsRoulette: install redux-logger and connect it

--- words-roulette/src/store/index.js
+++ words-roulette/src/store/index.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { createLogger } from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
 ////
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const logger = createLogger({
+  collapsed: true
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export { store };
